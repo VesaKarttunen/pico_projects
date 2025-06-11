dad38d50931b3a43d6558d9b147ddaf06f8363ce
Vesa Karttunen <vesakarttunen@hotmail.fi>
2025-06-11 20:41:59 +0300

    Add Hello world via USB

--- basic_demo/main.c
+++ basic_demo/main.c
@@ -1,15 +1,18 @@
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
+#include <stdio.h>
+
 void EnableLed(bool is_enabled) {
   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, is_enabled);
 }
 
 int main() {
-  int status = cyw43_arch_init();
-  hard_assert(status == PICO_OK);
+  cyw43_arch_init();
+  stdio_init_all();
 
   while (true) {
+    printf("Hello, world!\n");
     EnableLed(true);
     sleep_ms(500u);
     EnableLed(false);
