40c230b60538be049345b054b8fa7708007a7014
Vesa Karttunen <vesakarttunen@hotmail.fi>
2025-06-11 21:13:45 +0300

    Add onboard temperature printing

--- basic_demo/main.c
+++ basic_demo/main.c
@@ -1,7 +1,19 @@
+#include "hardware/adc.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
 #include <stdio.h>
+
+float ReadOnboardTemperature() {
+
+  /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+  const float conversionFactor = 3.3f / (1 << 12);
+
+  float adc = (float)adc_read() * conversionFactor;
+  float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+
+  return tempC;
+}
 
 void EnableLed(bool is_enabled) {
   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, is_enabled);
@@ -10,12 +22,16 @@ void EnableLed(bool is_enabled) {
 int main() {
   cyw43_arch_init();
   stdio_init_all();
+  adc_init();
+  adc_set_temp_sensor_enabled(true);
+  adc_select_input(4);
 
   while (true) {
-    printf("Hello, world!\n");
+
+    printf("Onboard temperature = %.02f C\n", ReadOnboardTemperature());
     EnableLed(true);
-    sleep_ms(500u);
+    sleep_ms(2000u);
     EnableLed(false);
-    sleep_ms(500u);
+    sleep_ms(2000u);
   }
 }
