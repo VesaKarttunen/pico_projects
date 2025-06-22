3b7f5f1f873aa7170499c404963252137f5f8df4
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-06-22 20:04:45 +0300

    Add Pico to create TCP socket server

---
+++ basic_demo/script/tcp_socket_client.py
@@ -0,0 +1,51 @@
+import socket
+import struct
+import threading
+import time
+import queue
+
+queue_send_message = queue.Queue()
+
+def GetUserMessageToSend():
+    while True:
+        message_to_send = input("Type message...\n")
+        queue_send_message.put(message_to_send)
+
+# Create and start the thread
+wait_keyboard_thread = threading.Thread(target=GetUserMessageToSend, daemon=True)
+wait_keyboard_thread.start()
+
+ip_address_pico  = "192.168.101.197"
+port_socket_pico = 1234
+
+# Connect TCP socket with configured settings
+socket_pico = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_pico.connect((ip_address_pico, port_socket_pico))
+
+def ReceiveDataFromTcpSocket():
+    while True:
+        size_recv_message_bytes = 11
+        data_received_raw = socket_pico.recv(size_recv_message_bytes)
+
+        # Convert received raw bits to actual message
+        message = struct.unpack("<11s", data_received_raw)
+        print("Received message from Pico: ", message)
+
+# Create and start the thread
+receive_feedback_thread = threading.Thread(target=ReceiveDataFromTcpSocket, daemon=True)
+receive_feedback_thread.start()
+
+def SendDataToTcpSocket():
+    while True:
+        if (not queue_send_message.empty()):
+            message = queue_send_message.get()
+            data_send = struct.pack("<64s", message.encode())
+            socket_pico.sendall(data_send)
+        else:
+            time.sleep(5.0)
+
+send_command_thread = threading.Thread(target=SendDataToTcpSocket, daemon=True)
+send_command_thread.start()
+
+while True:
+    pass
--- basic_demo/wifi_comm/wifi_comm.cpp
+++ basic_demo/wifi_comm/wifi_comm.cpp
@@ -4,8 +4,158 @@
 // Pico-SDK
 #include "pico/cyw43_arch.h"
 
+// lwIP
+#include "lwipopts.h"
+// Preven clan-format reordering
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "task.h"
+
 // STD
+#include <array>
 #include <cstdio>
+#include <string_view>
+
+static constexpr uint16_t f_socket_port = 1234u;
+
+// Own separate task is creater for each new connection
+static void TaskReceiveComm(void* p)
+{
+    // Socket descriptor value is just int number that is passed through void pointer
+    int socket = *static_cast<int*>(p);
+
+    while (true)
+    {
+        std::array<char, 64> message = {};
+
+        int count_bytes_received = lwip_read(socket,
+                                             message.data(),
+                                             sizeof(message));
+
+        if (count_bytes_received <= 0)
+        {
+            printf("Reading from the TCP socket failed. Closing the socket.\n");
+            break;
+        }
+        else if (static_cast<uint32_t>(count_bytes_received) == sizeof(message))
+        {
+            printf("Received message: %s\n", message.data());
+        }
+        else
+        {
+            printf("Received less bytes than expected from the socket. Discard message.\n");
+        }
+    }
+
+    lwip_close(socket);
+    vTaskDelete(nullptr);
+}
+
+// Own separate task is creater for each new connection
+static void TaskSendComm(void* p)
+{
+    // Socket descriptor value is just int number that is passed through void pointer
+    int socket = *static_cast<int*>(p);
+
+    TickType_t ticks_previous_wake = xTaskGetTickCount();
+
+    while (true)
+    {
+        xTaskDelayUntil(&ticks_previous_wake, pdMS_TO_TICKS(2000));
+
+        std::string_view message = "Hello world";
+
+        int count_bytes_sent = lwip_write(socket,
+                                          message.data(),
+                                          message.size());
+
+        if (count_bytes_sent <= 0)
+        {
+            printf("Writing into the TCP socket failed. Closing the socket.\n");
+            break;
+        }
+        else if (static_cast<unsigned>(count_bytes_sent) < message.size())
+        {
+            printf("Wrote less bytes than expected into the socket\n");
+            break;
+        }
+    }
+
+    lwip_close(socket);
+    vTaskDelete(nullptr);
+}
+
+static void TaskCreateSocket(void* p)
+{
+    (void)p;
+
+    int socket_listening = lwip_socket(AF_INET, SOCK_STREAM, 0);
+    bool is_error        = socket_listening < 0;
+
+    if (is_error)
+    {
+        printf("Creating listening socket failed\n");
+        return;
+    }
+
+    sockaddr_in address     = {};
+    address.sin_family      = AF_INET;
+    address.sin_port        = htons(f_socket_port);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    int status_bind = lwip_bind(socket_listening,
+                                reinterpret_cast<sockaddr*>(&address),
+                                sizeof(address));
+
+    is_error = status_bind < 0;
+
+    if (is_error)
+    {
+        printf("Binding socket failed\n");
+        return;
+    }
+
+    int status_listen = lwip_listen(socket_listening, 0);
+    is_error          = status_listen < 0;
+
+    if (is_error)
+    {
+        printf("Socket failed to start listening incoming connection\n");
+        return;
+    }
+
+    while (true)
+    {
+        int socket_new = lwip_accept(socket_listening, nullptr, nullptr);
+        is_error       = socket_new < 0;
+
+        // Successful new socket connection
+        if (!is_error)
+        {
+            printf("New socket connection accepted\n");
+
+            // Create tasks to handle sending and receiving data with the socket
+            sys_thread_new("TaskReceiveComm",
+                           TaskReceiveComm,
+                           &socket_new,
+                           DEFAULT_THREAD_STACKSIZE,
+                           2);
+
+            sys_thread_new("TaskSendComm",
+                           TaskSendComm,
+                           &socket_new,
+                           DEFAULT_THREAD_STACKSIZE,
+                           2);
+        }
+        else
+        {
+            printf("Socket failed to accept incoming connection\n");
+        }
+    }
+}
 
 // This init needs to be executed in a RTOS task because of it will also initialize
 // lwIP stack which requires it or the init will fail.
@@ -34,4 +184,13 @@ void WifiComm::Init()
     {
         printf("Wi-Fi connection failed\n");
     }
+
+    printf("Starting TCP socket server at:\n");
+    printf("IP: %s; Port %u\n", ip4addr_ntoa(netif_ip4_addr(netif_list)), f_socket_port);
+
+    sys_thread_new("TaskCreateSocket",
+                   TaskCreateSocket,
+                   nullptr,
+                   DEFAULT_THREAD_STACKSIZE,
+                   2);
 }
