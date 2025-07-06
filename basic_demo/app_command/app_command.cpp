d59c506a2c0656d70534543df0cdfbfbe0083493
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-07-06 17:18:56 +0300

    Revise command conf implementation

--- basic_demo/app_command/app_command.cpp
+++ basic_demo/app_command/app_command.cpp
@@ -1,42 +1,26 @@
 // Own
 #include "app_command.hpp"
-
-// Local-project
-#include "led/led.hpp"
+#include "command_conf.hpp"
 
 // STD
 #include <cstdio>
-
-using CommandHookFunc = void (*)(float);
-
-struct CommandHookMap
-{
-    std::string_view name;
-    CommandHookFunc hook_function;
-};
-
-// clang-format off
-static std::array<CommandHookMap, 2> command_conf{
-    CommandHookMap{"SetLedMode",        [](float mode)    { g_led.SetLedMode(static_cast<LedMode>(mode)); }},
-    CommandHookMap{"SetBlinkingPeriod", [](float period_s){ g_led.SetBlinkingPeriod_s(period_s); }}
-};
-// clang-format on
+#include <string_view>
 
 void AppCommand::ProcessCommand(const CommandFrame& requested_command)
 {
-    bool found_command = false;
+    bool found_valid_command = false;
 
-    for (auto available_command : command_conf)
+    for (auto available_command : CommandConf::GetSupportedCommands())
     {
         std::string_view requested_command_name = requested_command.name.data();
 
         if (available_command.name == requested_command_name)
         {
-            available_command.hook_function(requested_command.arqument);
-            found_command = true;
+            available_command.hook_func(requested_command.arqument);
+            found_valid_command = true;
         }
     }
-    if (!found_command)
+    if (!found_valid_command)
     {
         // NOLINTNEXTLINE(*stringview-data-usage)
         std::printf("Unknown command: %s\n", requested_command.name.data());
