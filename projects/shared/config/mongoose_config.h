b44393c0e1d6ab7f86c5cc12ed930ac8a4f7a801
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-07-25 13:32:00 +0300

    Add Mongoose web server library (v7.18)

---
+++ projects/shared/config/mongoose_config.h
@@ -0,0 +1,11 @@
+//---------------------------------------------------------------------------------------------------------------------
+// Mongoose Config
+//---------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#define MG_ARCH             MG_ARCH_PICOSDK
+#define MG_ENABLE_LWIP      1
+#define MG_ENABLE_PACKED_FS 1
+#define MG_ENABLE_POSIX_FS  0
+#define MG_OTA              MG_OTA_NONE
