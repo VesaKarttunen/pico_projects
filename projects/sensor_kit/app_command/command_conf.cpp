b617e8b81b207e42749c50aca743e572ad073c10
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-07-31 17:28:36 +0300

    Revise string to enum mapping of LedMode

--- projects/sensor_kit/app_command/command_conf.cpp
+++ projects/sensor_kit/app_command/command_conf.cpp
@@ -18,53 +18,27 @@
 #include <span>
 
 //---------------------------------------------------------------------------------------------------------------------
-// PRIVATE TYPE DEFINITIONS
-//---------------------------------------------------------------------------------------------------------------------
-
-struct LedModeMap
-{
-    std::string_view string;
-    LedMode enum_val;
-};
-
-//---------------------------------------------------------------------------------------------------------------------
-// PRIVATE CONSTANT DEFINITIONS
-//---------------------------------------------------------------------------------------------------------------------
-
-// clang-format off
-static constexpr std::array<LedModeMap, 5> f_led_mode_map{
-    {{.string = "ON",            .enum_val = LedMode::ON},
-     {.string = "OFF",           .enum_val = LedMode::OFF},
-     {.string = "BLINKING",      .enum_val = LedMode::BLINKING},
-     {.string = "BUTTON_PRESS",  .enum_val = LedMode::BUTTON_PRESS},
-     {.string = "BUTTON_TOGGLE", .enum_val = LedMode::BUTTON_TOGGLE}}
-};
-// clang-format on
-
-//---------------------------------------------------------------------------------------------------------------------
 // PRIVATE FUNCTION DEFINITIONS
 //---------------------------------------------------------------------------------------------------------------------
 
-static bool SetLedMode(std::string_view arg)
+static bool SetLedMode(std::string_view mode_str)
 {
-    for (auto mode : f_led_mode_map)
+    auto mode_enum = MapLedModeNameStrToEnum(mode_str);
+
+    if (mode_enum.has_value())
     {
-        if (mode.string == arg)
-        {
-            g_led.SetMode(mode.enum_val);
-
-            // Valid mode was found, thus search can be ended with early return
-            return true;
-        }
+        g_led.SetMode(mode_enum.value());
+        return true;
     }
-
-    // Did not find valid mode corresponding the arg
-    return false;
+    else
+    {
+        return false;
+    }
 }
 
-static bool SetLedBlinkingPeriod_s(std::string_view arg)
+static bool SetLedBlinkingPeriod_s(std::string_view period_str)
 {
-    auto period_s = ConvertStrToFloat(arg);
+    auto period_s = ConvertStrToFloat(period_str);
 
     bool is_arg_valid = period_s.has_value()
                      && (period_s.value() >= 0.0f);
@@ -83,12 +57,11 @@ static bool SetLedBlinkingPeriod_s(std::string_view arg)
 
 // clang-format off
 // NOLINTNEXTLINE(*interfaces-global-init)
-static std::array<CommandHookMap, 2> supported_commands{{
+static std::array<CommandHookMap, 2> f_supported_commands{{
     {.name      = "set_led_mode",
      .hook_func = SetLedMode},
     {.name      = "set_led_blinking_period_s",
-     .hook_func = SetLedBlinkingPeriod_s}
-}};
+     .hook_func = SetLedBlinkingPeriod_s}}};
 // clang-format on
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -97,5 +70,5 @@ static std::array<CommandHookMap, 2> supported_commands{{
 
 std::span<CommandHookMap> CommandConf::GetSupportedCommands()
 {
-    return supported_commands;
+    return f_supported_commands;
 }
--- projects/sensor_kit/led/led.hpp
+++ projects/sensor_kit/led/led.hpp
@@ -4,6 +4,12 @@
 
 #pragma once
 
+// STD
+#include <array>
+#include <optional>
+#include <string_view>
+#include <utility>
+
 //---------------------------------------------------------------------------------------------------------------------
 // PUBLIC TYPE DEFINITIONS
 //---------------------------------------------------------------------------------------------------------------------
@@ -16,6 +22,32 @@ enum struct LedMode
     BUTTON_PRESS  = 3,
     BUTTON_TOGGLE = 4
 };
+
+//---------------------------------------------------------------------------------------------------------------------
+// PUBLIC FUNCTION DEFINITIONS
+//---------------------------------------------------------------------------------------------------------------------
+
+inline constexpr std::optional<LedMode> MapLedModeNameStrToEnum(std::string_view name)
+{
+    // clang-format off
+    constexpr std::array<std::pair<std::string_view, LedMode>, 5> map_name_enum{{
+        {"OFF",           LedMode::OFF},
+        {"ON",            LedMode::ON},
+        {"BLINKING",      LedMode::BLINKING},
+        {"BUTTON_PRESS",  LedMode::BUTTON_PRESS},
+        {"BUTTON_TOGGLE", LedMode::BUTTON_TOGGLE}}};
+    // clang-format on
+
+    for (auto [valid_name, mapped_enum] : map_name_enum)
+    {
+        if (name == valid_name)
+        {
+            return mapped_enum;
+        }
+    }
+
+    return {};
+}
 
 //---------------------------------------------------------------------------------------------------------------------
 // PUBLIC CLASS DEFINITIONS
