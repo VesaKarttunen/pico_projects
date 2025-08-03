322c6c90140f472b9282467965d4d7c118da891e
Jussi Karttunen <jussi.m.karttunen@gmail.com>
2025-08-03 12:54:20 +0300

    Improve packed file system generation script

--- projects/sensor_kit/script/generate_packed_file_system.py
+++ projects/sensor_kit/script/generate_packed_file_system.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 # When executed as a script, call as follows:
@@ -6,31 +7,42 @@ import sys
 web_page_source_dir = sys.argv[1]
 generated_packed_file_system_cpp_file = sys.argv[2]
 
-template_packed_file_system = web_page_source_dir + "/web_page_packed_file_system.tpp"
+# Note that the script assumes that the template file for generating the packed files system Cpp file is:
+# -Named:   web_page_packed_file_system.tpp
+# -Located: direct parent directory of the web page source directory
+# TODO: Fix these hard coded assumptions
+template_packed_file_system = web_page_source_dir + "/../web_page_packed_file_system.tpp"
 template_file = open(template_packed_file_system, "r")
 template_content = template_file.read()
 template_file.close()
 
-web_page_source_file_names = ["/index.html", "/main.js", "/style.css"]
+source_file_names = os.listdir(web_page_source_dir)
 
-for file_name in web_page_source_file_names:
-    source_file = open(web_page_source_dir + file_name, "r")
+# Cpp code snippet templates to generate packed files system source file content
+array_def_template = "static const unsigned char {array_name}[] = {{{unicode_content}}};\n\n"
+info_row_def_template = "{{.name = \"/{file_name}\", .data = {array_name}, .size = sizeof({array_name}), .mtime = 1700000000}},\n"
+
+# These strings will hold the final substituted content replacing the placeholders in the template file
+array_defs_subst = ""
+info_row_defs_subst = ""
+
+for file_name in source_file_names:
+    source_file = open(web_page_source_dir + "/" + file_name, "r")
     source_content = source_file.read()
     source_file.close()
 
     unicode_content = ""
-    for index, character in enumerate(source_content):
+    for character in source_content:
         unicode_content += str(ord(character)) + ", "
-        if index % 20 == 0 and index != 0:
-            unicode_content += "\n"
     unicode_content += "0"
 
-    if file_name == "/index.html":
-        template_content = template_content.replace("@<index_html_unicode_character_content>", unicode_content)
-    elif file_name == "/main.js":
-        template_content = template_content.replace("@<main_js_unicode_character_content>", unicode_content)
-    elif file_name == "/style.css":
-        template_content = template_content.replace("@<style_css_unicode_character_content>", unicode_content)
+    array_name = file_name.replace(".","_")
+    array_defs_subst += array_def_template.format(array_name=array_name, unicode_content=unicode_content)
+    info_row_defs_subst += info_row_def_template.format(file_name=file_name, array_name=array_name)
+
+# Replace the placeholders in the template file with the generated substitution content
+template_content = template_content.replace("@<array_defs>", array_defs_subst)
+template_content = template_content.replace("@<info_row_defs>", info_row_defs_subst)
 
 generated_file = open(generated_packed_file_system_cpp_file, "w")
 generated_file.write(template_content)
--- projects/sensor_kit/web_server/web_page_packed_file_system.tpp
+++ projects/sensor_kit/web_server/web_page_packed_file_system.tpp
@@ -1,30 +1,18 @@
 #include "mongoose/mongoose.h"
 
-// clang-format off
-static const unsigned char index_html[] = {
-@<index_html_unicode_character_content>
-};
+// Read-only file system is emulated with embedding the content of the files into firmware binary
+// as unicode character values served from build time generated const arrays
+@<array_defs>
 
-static const unsigned char main_js[] = {
-@<main_js_unicode_character_content>
-};
-
-static const unsigned char style_css[] = {
-@<style_css_unicode_character_content>
-};
-// clang-format on
-
-static const struct packed_file
+static struct packed_file
 {
     const char* name;
     const unsigned char* data;
     size_t size;
     time_t mtime;
 } packed_files[] = {
-    {.name = "/index.html", .data = index_html, .size = sizeof(index_html), .mtime = 1'753'533'813},
-    {.name = "/main.js",    .data = main_js,    .size = sizeof(main_js),    .mtime = 1'753'534'790},
-    {.name = "/style.css",  .data = style_css,  .size = sizeof(style_css),  .mtime = 1'753'534'006},
-    {.name = nullptr,       .data = nullptr,    .size = 0,                  .mtime = 0            }
+@<info_row_defs>
+{.name = nullptr, .data = nullptr, .size = 0, .mtime = 0}
 };
 
 static int scmp(const char* a, const char* b)
